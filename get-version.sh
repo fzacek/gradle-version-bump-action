e55166f80e62510f193a99d8296aaa16c01c4fe9
g-marshall-img-arena <82041919+g-marshall-img-arena@users.noreply.github.com>
2021-07-15 21:52:47 +0100

    Update get-version.sh
    # version element is prefixed by 4 spaces as this corresponds to the project version and ignores e.g. parent versions

--- get-version.sh
+++ get-version.sh
@@ -1,1 +1,2 @@
-cat $POMPATH/pom.xml | grep "<version>.*</version>" | head -1 | awk -F'[><]' '{print $3}'
+# version element is prefixed by 4 spaces as this corresponds to the project version and ignores e.g. parent versions
+cat $POMPATH/pom.xml | grep "^    <version>.*</version>" | head -1 | awk -F'[><]' '{print $3}'
